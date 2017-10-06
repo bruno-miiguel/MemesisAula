d171a5e04341a6245e41cdd3ae602ef550d9bd28
Aluno <Aluno>
2017-10-05 22:20:38 -0300

    Second

--- Memesis/Controllers/AccountController.cs
+++ Memesis/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Memesis.Models;
+using Memesis.Models.Negocios;
 
 namespace Memesis.Controllers
 {
@@ -156,12 +157,19 @@ namespace Memesis.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    var db = new ContextoDb();
+                    var userModel = new UsuarioModel();
+                    userModel.Email = user.Email;
+                    db.Usuarios.Add(userModel);
+
+                    db.SaveChanges();
 
                     return RedirectToAction("Index", "Home");
                 }
